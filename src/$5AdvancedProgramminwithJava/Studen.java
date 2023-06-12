5aa096e0c81c332b8571a78b0d1fe7c062200b5b
Nguyenanhtuan1997 <nguyenanhtuan041297@gmail.com>
2023-06-12 08:11:11 +0700

    up

---
+++ src/$5AdvancedProgramminwithJava/Studen.java
@@ -0,0 +1,23 @@
+package $5AdvancedProgramminwithJava;
+
+public class Studen {
+    private String name;
+    private String classes;
+
+    public Studen() {
+        this.name = "John";
+        this.classes = "C02";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+
+    }
+
+
